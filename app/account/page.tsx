c676c7bbdeba43841d374f916bff30333ce908e5
MOHAMED <mohamed1239455@gmail.com>
2024-06-23 20:42:17 +0200

    profile page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -4,6 +4,10 @@ export const metadata = {
   title: 'Gest area',
 };
 
-export default function page() {
-  return <h1>Account page</h1>;
+export default function Account() {
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, mohamed
+    </h2>
+  );
 }
